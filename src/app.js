ce74736176189a7e5f3862e273b80c96d1f1ba5e
spk-v <saipavankalyaan97@gmail.com>
2020-11-18 20:22:36 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,81 @@
+const express=require('express')
+const app=express()
+const path=require('path')
+const hbs=require('hbs')
+const forecast=require('./utils/forecast')
+
+//Define paths for Express config
+const publicDirectoryPath=path.join(__dirname,'../public')
+const viewsPath=path.join(__dirname,'../templates/views')
+const partialsPath=path.join(__dirname,'../templates/partials')
+
+//setup handlerbars engine and views location
+app.set('view engine', 'hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static Directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        name:'SPK',
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Me',
+        name:'SPK'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:"Help",
+        message:'This is a Help message',
+        name:'SPK'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.city){
+        return res.send({
+            error:'You must provide a city'
+        })
+    }
+    forecast(req.query.city,(error,data)=>{
+        if(error==undefined){
+            return res.send({
+                forecast:data,
+                location:req.query.city
+            })
+        }
+        res.send({
+            error
+        })
+        
+    })
+    
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('page404',{
+        title:'404',
+        message:'Help article not found.',
+        name:'SPK'
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('page404',{
+        title:'404',
+        message:'Page not found.',
+        name:'SPK'
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('server is running at port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request=require('request')
+
+const forecast=(city,callback)=>{
+    const url='http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=018e4898874da81767023d19f4851ffd&units=metric'
+request({url,json:true}, (error,{body})=>{
+    // fs.writeFileSync('weather.json',JSON.stringify(response.body))
+    if(error){
+        callback('Unable to connect to the weather service!',undefined)
+    }else if(body.message){
+        callback(body.message,undefined)
+    }else{
+        callback(undefined,'It is currently ' + body.main.temp + ' degrees out. Expected ' + body.weather[0].description + '.')
+    }
+    
+})
+}
+module.exports=forecast
